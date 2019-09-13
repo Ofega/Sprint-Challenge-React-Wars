37ed7112689cfce6c6fa6e110046a5d137949634
ofega <fegaeze@gmail.com>
2019-09-13 10:16:47 +0100

    added loading indicator

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -7,20 +7,27 @@ import Characters from './components/Characters';
 const App = () => {
 
   const [ characters, setCharacters ] = useState([]);
+  const [isLoading, setLoadingIndicator] = useState(true);
+  const [ pagination, setPagination ] = useState({})
 
   useEffect(() => {
     axios.get('https://swapi.co/api/people/')
       .then((response) => {
         setCharacters(response.data.results);
+        setPagination({
+          next: response.data.results.next,
+          previous: response.data.results.previous
+        });
+        setLoadingIndicator(false);
       })
-
   }, [])
 
   return (
-    <Main>
-      <h1><img src={logo} alt="Starwars Logo" /></h1>
-
-      <Characters data={characters} />
+    <Main onScroll={() => console.log('scrolled')}>
+      <h1>
+        <img src={logo} alt="Starwars Logo" />
+      </h1>
+      <Characters data={characters} isLoading={isLoading} />
     </Main>
   );
 }
@@ -29,13 +36,21 @@ const App = () => {
 // Styling
 const Main = styled.div`
   height: 100%;
+  width: 100%;
   max-width: 1140px;
   margin: 0 auto;
-  padding: 3rem 2rem;
+  padding: 3rem;
   font-size: 1.5rem;
 
   h1 {
     text-align: center;
+    max-width: 400px;
+    margin: 0 auto;
+
+    img {
+      width: 100%;
+      height: 100%;
+    }
   }
 `
 
--- starwars/src/components/Characters.js
+++ starwars/src/components/Characters.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
+import LoadingIndicator from "./LoadingIndicator";
 
 const Character = props => {
 	const { name } = props;
 	return (
-		<Item>{name}</Item>
+		<Item>
+			<h2>{name}</h2>
+		</Item>
 	)
 }
 
 const Characters = props => {
-	const { data } = props;
+	const { data, isLoading } = props;
 
 	return (
 		<List>
-			{ data.map(item => <Character key={item.name} name={item.name} />) }
+			{ 
+					isLoading ? 
+						<LoadingIndicator /> :
+						data.map(item => <Character key={item.name} name={item.name} />) 
+			}
 		</List>
 	)
 }
@@ -23,13 +30,26 @@ const Characters = props => {
 const List = styled.ul`
 	list-style-type: none;
 	display: flex;
-	justify-content: space-between;
+	justify-content: center;
 	flex-wrap: wrap;
+	max-width: 300px;
+	margin: 0 auto;
+	min-height: 300px;
+
+	@media (min-width: 500px) {
+		max-width: 500px;
+		justify-content: space-between;
+	}
+
+	@media (min-width: 768px) {
+		max-width: 100%;
+	}
 `
 
 const Item = styled.li`
 	list-style-type: none;
-	width: calc(25% - 4rem);
+	width: 100%;
+	max-width: 500px;
 	min-height: 300px;
 	margin-top: 6rem;
   position: relative;
@@ -38,7 +58,23 @@ const Item = styled.li`
   background: #000;
   background-clip: padding-box;
   border: solid -5px transparent;
-  border-radius: 1em;
+	border-radius: 1em;
+	padding: 1rem 1.5rem;
+	display: flex;
+	flex-direction: column;
+	align-items: center
+	
+	@media (min-width: 500px) {
+		width: calc(50% - 2rem);
+	}
+
+	@media (min-width: 768px) {
+		width: calc(33.33% - 3rem);
+	}
+
+	@media (min-width: 1024px) {
+		width: calc(33.33% - 4rem);
+	}
 
   &:before {
     content: '';
@@ -48,7 +84,11 @@ const Item = styled.li`
     margin: -5px;
     border-radius: inherit;
     background: linear-gradient(to right, red, orange);
-  }
+	}
+	
+	h2 {
+		font-size: 2rem;
+	}
 `
 
 export default Characters;
